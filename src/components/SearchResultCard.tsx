36cce5b2fc01f30bf912f739630eaf88bb9feb98
Mitva <115395431+MitvaVirvadiya@users.noreply.github.com>
2024-03-08 19:22:09 +0530

    search page layout - restaurant card added

---
+++ src/components/SearchResultCard.tsx
@@ -0,0 +1,55 @@
+import { Restaurant } from "@/types";
+import { Link } from "react-router-dom";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { Banknote, Clock, Dot } from "lucide-react";
+
+type Props = {
+  restaurant: Restaurant;
+};
+
+const SearchResultCard = ({ restaurant }: Props) => {
+  return (
+    <Link
+      to={`/restaurant/${restaurant._id}`}
+      className="grid lg:grid-cols-[2fr_3fr] gap-5 group"
+    >
+      <AspectRatio ratio={16 / 6}>
+        <img
+          src={restaurant.imageUrl}
+          className="w-full h-full rounded-md object-cover"
+        />
+      </AspectRatio>
+      <div>
+        <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline">
+          {restaurant.restaurantName}
+        </h3>
+        <div id="card-content" className="grid md:grid-cols-2 gap-2">
+          <div className="flex flex-row flex-wrap">
+            {restaurant.cuisines.map((cuisine, index) => (
+              <span className="flex">
+                <span>{cuisine}</span>
+                {index < restaurant.cuisines.length - 1 && (
+                  <span>
+                    <Dot />
+                  </span>
+                )}
+              </span>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-1 text-green-600">
+              <Clock className="text-green-600" />
+              {restaurant.estimatedDeliveryTime} mins
+            </div>
+            <div className="flex items-center gap-1">
+              <Banknote />
+              Delivery from ₹{restaurant.deliveryPrice}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default SearchResultCard;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,22 +1,33 @@
 import { usesearchRestaurants } from "@/api/restaurantApi";
+import Loading from "@/components/Loading";
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { restaurants } = usesearchRestaurants(city);
+  const { restaurants, isLoading } = usesearchRestaurants(city);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!restaurants?.data || !city) {
+    return <span>No Data Found</span>;
+  }
 
   return (
-    <div>
-      <h1>
-        user search by {city} -{" "}
-        <span>
-          {restaurants?.data.map((restaurant) => (
-            <span>
-              {restaurant.restaurantName}, {restaurant.city}{" "}
-            </span>
-          ))}
-        </span>{" "}
-      </h1>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div>Cuisines here</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <div className="flex justify-between flex-col gap-3 lg:flex-row">
+            <SearchResultInfo total={restaurants.pagination.total} city={city}/>
+            <h1>Dropdown here</h1>
+        </div>
+        {restaurants.data.map((restaurant) => (
+            <SearchResultCard restaurant={restaurant} />
+        ))}
+      </div>
     </div>
   );
 };
